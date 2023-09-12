f9f92e63a40036d78666575180678bab51ab9bf8
Natalie <natalie.ann.kwak@gmail.com>
2023-09-11 23:40:06 -0400

    restart

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+    <h1>
+      Looking for Natalie Kwak's Personal Site? You're in the right place! This page needs to be updated, check back soon :<span>&#41;</span>
+    </h1>
+    <break></break>
+    <h2>While you wait, why don't you check out Natalie's <a href="https://www.linkedin.com/in/natalie-kwak/">LinkedIn</a> or <a href="https://github.com/nataliekwak">GitHub</a>?</h2>
+    </div>
+  );
+}
+
+export default App;
