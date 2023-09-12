faf33076bb543edf0d8c4486bcae606b7015ed95
Natalie <natalie.ann.kwak@gmail.com>
2023-09-11 22:54:38 -0400

    initial commit/probably only commit

---
+++ tester/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1>
+       Looking for Natalie Kwak's Personal Site? You're in the right place! This page needs to be updated, check back soon :<span>&#41;</span>
+    </h1>
+  );
+}
+
+export default App;
