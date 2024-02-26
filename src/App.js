3d508168667b7e565850e40922ec0db89c4c0722
Natalie <natalie.ann.kwak@gmail.com>
2024-02-27 10:22:12 +1100

    updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
       Looking for Natalie Kwak's Personal Site? You're in the right place! This page needs to be updated, check back soon :<span>&#41;</span>
     </h1>
     <break></break>
-    <h2>While you wait, why don't you check out Natalie's <a href="https://www.linkedin.com/in/natalie-kwak/">LinkedIn</a>, <a href="https://github.com/nataliekwak">GitHub</a>, or <a href="https://nataliekwak.com/NK_Resume23.pdf">resume</a>?</h2>
+    <h2>While you wait, why don't you check out Natalie's <a href="https://www.linkedin.com/in/natalie-kwak/">LinkedIn</a>, <a href="https://github.com/nataliekwak">GitHub</a>, or <a href="https://nataliekwak.com/Resume_NK24.pdf">resume</a>?</h2>
     </div>
   );
 }
