df950b5b458a03dde53efae54db1a158101e9930
Natalie <natalie.ann.kwak@gmail.com>
2024-09-16 15:10:21 -0400

    text update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
     <h1>
-      Looking for Natalie Kwak's Personal Site? You're in the right place! This page needs to be updated, check back soon :<span>&#41;</span>
+      Looking for Natalie Kwak's Personal Site? You're in the right place! This page is currently being updated, check back soon :<span>&#41;</span>
     </h1>
     <break></break>
     <h2>While you wait, why don't you check out Natalie's <a href="https://www.linkedin.com/in/natalie-kwak/">LinkedIn</a>, <a href="https://github.com/nataliekwak">GitHub</a>, or <a href="https://nataliekwak.com/KwakN_Fall24.pdf">resume</a>?</h2>
